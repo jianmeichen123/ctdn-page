1d4b0a121861353247f85403cb2098577ebc800e
vincent <leizhang>
2017-05-05 14:07:41 +0800

    pom

--- js/project_query.js
+++ js/project_query.js
@@ -2,12 +2,32 @@ function _query(){
     var querydata = {}
     $("[data-query]").each(function(i,e){
         var o = $(e)
-        if(o.attr("data-query").split(":")[1]="tree"){
+        var type = o.attr("data-query").split(":")[1]
+        if(type=="normal"||type=="nor"){
             var name = o.attr("data-query").split(":")[0]
             querydata[name] = []
             o.find(".pick_on").each(function(j,f){
                 if($(f).attr("data-id")){
                 querydata[name].push($(f).attr("data-id"))
+                }
+            })
+
+        }else if(type=="district"){
+
+            var name = o.attr("data-query").split(":")[0]
+                if (!querydata[name]){
+                    querydata[name] = []
+                    querydata[name+"Sub"] = []
+                }
+
+            o.find(".pick_on").each(function(j,f){
+                if($(f).attr("data-id")){
+                    if(f.hasClass("global_mousemove")){
+                        querydata[name].push($(f).attr("data-id"))
+                    }else{
+                        querydata[name+"Sub"].push($(f).attr("data-id"))
+                    }
+
                 }
             })
 
