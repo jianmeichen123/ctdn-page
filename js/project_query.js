78f23d992c2c9b10d7e4c9568afb58bb19bbd18f
wanghaonan <haonanwang@galaxyinternet.com>
2017-05-05 13:42:33 +0800

    rest

--- js/project_query.js
+++ js/project_query.js
@@ -2,7 +2,7 @@ function _query(){
     var querydata = {}
     $("[data-query]").each(function(i,e){
         var o = $(e)
-        if(o.attr("data-query")){
+        if(o.attr("data-query").split(":")[1]="tree"){
             var name = o.attr("data-query").split(":")[0]
             querydata[name] = []
             o.find(".pick_on").each(function(j,f){
@@ -10,10 +10,15 @@ function _query(){
                 querydata[name].push($(f).attr("data-id"))
                 }
             })
+
         }
 
     })
 
+    var projTitle = $("input#projTitle").val();
+    var title = "projTitle";
+    querydata[title] = []
+    querydata[title].push(projTitle)
     console.log(querydata)
     /*sendPostRequestByJsonObj("http://10.9.130.143:8081/api/search/project", querydata, function(cData){console.log(data)})*/
 }
