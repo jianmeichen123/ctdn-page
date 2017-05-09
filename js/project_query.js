41dc8fa307e7492ec1994e7781bbb1e2b780782c
wanghaonan <haonanwang@galaxyinternet.com>
2017-05-09 11:36:47 +0800

    rest

--- js/project_query.js
+++ js/project_query.js
@@ -66,6 +66,7 @@ function _query(){
     }else{
         querydata["beginDate"] = d1;
         querydata["endDate"] = d2;
+        return false;
     }
 
     querydata["projTitle"] = $("#projTitle").val();
