132b5236e0d619f0579d2361201893703392ac65
vincent <leizhang>
2017-05-17 14:10:24 +0800

    国内切换点击事件

--- js/list_page.js
+++ js/list_page.js
@@ -302,7 +302,7 @@ $('body').delegate('[data-query="listingTypeSubIds:nor"] .condition_all_ul li','
 })
 
 //国内切换点击事件
-/*$('body').delegate('[data-query="districtIds:district"] li','click', function(event){
+$('body').delegate('[data-query="districtIds:district"] li','click', function(event){
 	event.stopPropagation(); 
 	var click_this =$(this).index();
 	var click_id = $(this).attr('global-id')
@@ -315,7 +315,7 @@ $('body').delegate('[data-query="listingTypeSubIds:nor"] .condition_all_ul li','
 		$("[global-list='"+click_id+"'] li").first().addClass('pick_on');
 		$('#global_all li').removeClass('pick_on_color');
 		$(".block_list .condition_all_ul li").removeClass('pick_on');
-		*//*$(".block_list .condition_all_ul").each(function(i){
+		/*$(".block_list .condition_all_ul").each(function(i){
 			$(this).children('li').first().addClass('pick_on');
 		});*//*
 		$(this).addClass('pick_on');
