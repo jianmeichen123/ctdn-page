df72b9abc184413eb2f75bdc3e70e19445475bd1
vincent <leizhang>
2017-05-18 11:15:44 +0800

    dev

--- js/platformUrl.js
+++ js/platformUrl.js
@@ -1,4 +1,5 @@
 //本地
+/*
 var Constants = {
 	platformContentURL: "http://10.9.130.143:8081/api/",
 	userContentURL: "http://ctdndev.gi.com/user/",
@@ -9,8 +10,10 @@ var Constants = {
 var home = {
 	    //;index : "http://ctdn.galaxyinternet.com"
 	}
+
+*/
 //线上
-/*var Constants = {
+var Constants = {
 	platformContentURL: "http://ctdndev.gi.com/api/",
 	userContentURL: "http://ctdndev.gi.com/user/",
 	searchContentURL: "http://ctdndev.gi.com/search/",
@@ -19,7 +22,7 @@ var home = {
 }
 var home = {
 	    index : "http://ctdn.galaxyinternet.com"
-	}*/
+	}
 var docUrl = {
 
       /**
