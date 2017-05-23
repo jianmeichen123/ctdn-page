f3b970d3d717dcf849a6c068a615d6cdac7c5ea7
vincent <leizhang>
2017-05-23 14:26:29 +0800

    ctdndev.gi.com

--- js/platformUrl.js
+++ js/platformUrl.js
@@ -1,19 +1,16 @@
 //本地
 
-var Constants = {
+/*var Constants = {
 	platformContentURL: "http://10.9.130.143:8081/api/",
 	userContentURL: "http://ctdndev.gi.com/user/",
 	searchContentURL: "http://127.0.0.1:8089/search/",
 	dataContentURL: "http://ctdndev.gi.com/data/",
 	logoPath: "http:///10.10.0.147/"
-}
-var home = {
-	    //;index : "http://ctdn.galaxyinternet.com"
-	}
+}*/
 
 
 //线上
-/*var Constants = {
+var Constants = {
 	platformContentURL: "http://ctdndev.gi.com/api/",
 	userContentURL: "http://ctdndev.gi.com/user/",
 	searchContentURL: "http://ctdndev.gi.com/search/",
@@ -21,8 +18,8 @@ var home = {
     logoPath: "http:///10.10.0.147/"
 }
 var home = {
-	    index : "http://ctdn.galaxyinternet.com"
-	}*/
+	    index : "http://ctdndev.gi.com"
+	}
 var docUrl = {
 
       /**
