959fd5bbe2ccd802e1e49b2068b473577be08c33
wanghaonan <haonanwang@galaxyinternet.com>
2017-06-14 14:16:19 +0800

    rest

--- js/jg_particulars.js
+++ js/jg_particulars.js
@@ -1,2 +1,66 @@
 
+//投资事件
+function eventInfoListFormatter(data,div){
+     var staticTemplate =  "<tr><td>${investSideJson}</td><td>${company}</td><td>${investDate}</td><td>${round}</td><td> ${amountStr}</td><td>${eventId}</td></tr>";
+     var temp = staticTemplate;
+     var html = "";
+     //遍历数组
+     $(data).each(function(i,row){
+        $.each(row,function(k,v){
+            while(temp.indexOf("${"+k+"}") > 1){
+                if(v){
+                    if(k =="investSideJson"){
+                        var json = eval("(" + v + ")");
+                        var ls = json["investSideJson"];
+                        var firms = "";
+                        for(i in ls){
+                            var json = ls[i]
+                            if(i<3){
+                                firms += "<a href = '#?id="+json.id+"'>"+json.invstor+"</a><br>";
+                            }
+                        }
+//                        $(ls).each(function(){
+//                           //待修改 没加领投
+//                           firms += "<a href = '#?id="+$(this)[0].id+"'>"+$(this)[0].invstor+"</a><br>";
+//                        })
+                        v = firms
+                    }
+                    if(k == "eventId"){
+                         //待修改 跳转事件详情
+                         v = "<a href='#?id="+row.eventId+"'>详情</a>"
+                    }
+                    if(k =="investDate"){
+                        v = formatDate(v, "yyyy-MM-dd")
+                    }
+                }else{
+                    v= "-"
+                }
+                temp =temp.replace("${"+k+"}",v)
+            }
+        })
+        html += temp;
+        temp = staticTemplate
+     })
+     div.append(html)
+}
+//相关新闻
+function orgMediaInfoListFormatter(data,div){
+   var staticTemplate = '<tr> <td class="one">${title}</td> <td class="two">${content}</td> <td class="three">${eventDate}</td> </tr>'
+   var temp = staticTemplate;
+    var html =""
+    $(data).each(function(i,row){
+         $.each(row,function(k,v){
+             while(temp.indexOf("${"+k+"}") > 1){
+                if(k =="eventDate"){
+                    v = formatDate(v, "yyyy-MM-dd")
+                }
+                if(!v){ v = "-"}
+                temp = temp.replace("${"+k+"}",v)
+             }
+         })
+         html += temp;
+         temp = staticTemplate
+    })
+    div.append(html)
+}
 sendGetRequest(detail.queryInvestOrgInfo+"1",function(data){fillOne(data.data,$("div[data-query='baseOrgInfo']")); fillList(data.data,$("*[data-query='list']"))})
