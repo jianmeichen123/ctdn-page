ed0412986f845e68db434dff472a5aa48f2f6ec8
wanghaonan <haonanwang@galaxyinternet.com>
2017-06-14 10:54:49 +0800

    rest

--- js/jg_particulars.js
+++ js/jg_particulars.js
@@ -1,2 +1,2 @@
 
-sendGetRequest(detail.queryInvestOrgInfo+"1",function(data){fillOne(data.data[0],$("div[data-query='baseOrgInfo']")); fillList(data.data,$("*[data-query='list']"))})
+sendGetRequest(detail.queryInvestOrgInfo+"1",function(data){fillOne(data.data,$("div[data-query='baseOrgInfo']")); fillList(data.data,$("*[data-query='list']"))})
