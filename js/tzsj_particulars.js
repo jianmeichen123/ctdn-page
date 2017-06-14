444fb05ce2465db30eb884ff760b851f6a3f4a5e
wanghaonan <haonanwang@galaxyinternet.com>
2017-06-14 17:43:11 +0800

    rest

--- js/tzsj_particulars.js
+++ js/tzsj_particulars.js
@@ -5,10 +5,24 @@ function fillBaseEventInfo(data,divList){
         var ls = div.find("*[data-field]")
         $(ls).each(function(){
             var o = $(this);
-            if(data[o.attr("data-field")]){
-                o.html(data[o.attr("data-field")])
+            var k = o.attr("data-field")
+            var v = data[o.attr("data-field")]
+            if(k=='investDate'){
+                v = formatDate(v, "yyyy-MM-dd")
+            }
+            if(k=='desc'&&!v){
+                v = '暂无描述'
+            }
+            if(k=='industrySubName'&&v){
+                v = '>'+v
+            }
+            if(k=='investevent'){
+                v = '未透露'
+            }
+            if(v){
+                o.html(v)
             }else{
-                o.html("未知")
+                o.html("-")
             }
         })
     })
