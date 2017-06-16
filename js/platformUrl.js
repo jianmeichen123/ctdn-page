3eccb1b2f8826d44d26e943e59c0873e6431faa1
chunhuanzhang <chunhuanzhang@galaxyinternet.com>
2017-06-16 10:14:32 +0800

    url

--- js/platformUrl.js
+++ js/platformUrl.js
@@ -1,23 +1,23 @@
 //本地
 
-var Constants = {
-	platformContentURL: "http://127.0.0.1:8081/api/",
-	userContentURL: "http://ctdndev.gi.com/user/",
-	searchContentURL: "http://10.9.130.135:8089/search/",
-	dataContentURL: "http://ctdndev.gi.com/data/",
-	logoPath: "http:///10.10.0.147/"
-}
+//var Constants = {
+//	platformContentURL: "http://127.0.0.1:8081/api/",
+//	userContentURL: "http://ctdndev.gi.com/user/",
+//	searchContentURL: "http://10.9.130.135:8089/search/",
+//	dataContentURL: "http://ctdndev.gi.com/data/",
+//	logoPath: "http:///10.10.0.147/"
+//}
 
 
 
 //线上
-//var Constants = {
-//	platformContentURL: "http://127.0.0.1:8081/api/",
-//	userContentURL: "http://ctdndev.gi.com/user/",
-//	searchContentURL: "http://ctdndev.gi.com/search/",
-//	dataContentURL: "http://ctdndev.gi.com/data/",
-//    logoPath: "http:///10.10.0.147/"
-//}
+var Constants = {
+	platformContentURL: "http://127.0.0.1:8081/api/",
+	userContentURL: "http://ctdndev.gi.com/user/",
+	searchContentURL: "http://ctdndev.gi.com/search/",
+	dataContentURL: "http://ctdndev.gi.com/data/",
+    logoPath: "http:///10.10.0.147/"
+}
 var home = {
 	    index : "http://ctdndev.gi.com"
 	}
