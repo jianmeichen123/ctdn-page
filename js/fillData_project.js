f06dc2e81a4367d6842cfdd2029a16ab9b7721df
chunhuanzhang <chunhuanzhang@galaxyinternet.com>
2017-06-20 15:18:30 +0800

    yangshi

--- js/fillData_project.js
+++ js/fillData_project.js
@@ -139,8 +139,8 @@ function projectContactListFormatter(data,div){
 //上市挂牌
 function eventListedInfoListFormatter(data,div){
    var staticTemplate = '<tr> <td style=""> <div class="list_table_td"> <img height="37" width="37" src="${logo}"> <ul class="col_999"> <li><a href="#">${projTitle}</a></li> <li><span>${districtSubName}</span><span>${industryName}</span></li> </ul> </div> </td> <td>${type}</td> <td>${stockExchange}</td> <td>${stockCode}</td> <td>${listedDate}</td> <td>${eventId}</td> </tr>'
-    var temp = staticTemplate;
-    var html = "";
+   var temp = staticTemplate;
+   var html = "";
     //遍历数组
     if(data.length>0){
            $(data).each(function(i,row){
