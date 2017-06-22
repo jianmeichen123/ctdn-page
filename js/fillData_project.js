17dfebce34888226922c4e0f67d799e5ab1735b6
chunhuanzhang <chunhuanzhang@galaxyinternet.com>
2017-06-22 11:04:31 +0800

    ls

--- js/fillData_project.js
+++ js/fillData_project.js
@@ -211,9 +211,11 @@ function eventMergerInfoListFormatter(data,div){
                           var json = eval("(" + v + ")");
                           var ls = json["mergeSideJson"];
                           var firms = "";
-                          $(ls).each(function(){
+                          $(ls).each(function(i){
                              //待修改 没加领投
-                             firms += "<a href = 'jg_particulars.html?orgId="+$(this)[0].id+"'>"+$(this)[0].title+"</a>";
+                             if(i<3){
+                                firms += "<div><a href = 'jg_particulars.html?orgId="+$(this)[0].id+"'>"+$(this)[0].invstor+"</a></div>";
+                             }
                           })
                           v = firms
                       }
