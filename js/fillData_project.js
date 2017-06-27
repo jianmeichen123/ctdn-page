2033fdaf73e343c874193642927d613864a7c893
chunhuanzhang <chunhuanzhang@galaxyinternet.com>
2017-06-27 17:49:51 +0800

    weixin

--- js/fillData_project.js
+++ js/fillData_project.js
@@ -424,8 +424,9 @@ var callBack = {
                 }
            }else if(k=="weixin"){
                if(v){
+               o.parent().append('<ul class="weixin_ewm"><li class="wx_img"><img src="https://open.weixin.qq.com/qr/code/?username='+v+'"><span>'+v+'</span>	</li> </ul>')
                 v='<span class="brain_ico brain_ico_project_2_on"  ></span>'
-                o.parent().append('<ul class="weixin_ewm"><li class="wx_img"><img src="https://open.weixin.qq.com/qr/code/?username='+v+'"><span>'+v+'</span>	</li> </ul>')
+
                }else{
                 v='<span class="brain_ico brain_ico_project_2"  title="" ></span>'
                }
