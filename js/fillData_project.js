0f7c0c3920cfd6a9974c1169dd4734954e626004
chunhuanzhang <chunhuanzhang@galaxyinternet.com>
2017-07-12 14:04:24 +0800

    优化页面

--- js/fillData_project.js
+++ js/fillData_project.js
@@ -326,19 +326,6 @@ $("#product-ul").on("click","li[op-data-type]",function(){
     var _name= $self.attr("data-name");
     var id = $self.attr("id").split(":")[0];
     var id_type = $self.attr("id").split(":")[1];
-    //自定页
-//	layer.open({
-//        type: 2,
-//        title: _name,
-//        shadeClose: true,
-//        maxmin: true, //开启最大化最小化按钮
-//        area: ['900px', '435px'],
-//        content: _url,
-//        success: function(layero, index){
-//            $("#popup_name").html(_name);
-//            sendPostRequest(dataUrl[$self.attr("op-data-type")]+id,$(layero).find("iframe")[0].contentWindow.opCallBack[$self.attr("op-data-type")]);
-//        }
-//    });
     return false;
 });
 
@@ -370,106 +357,7 @@ var callBack = {
         }
 }
 
-//企业基本信息formatter
- function formatProjectInfo(data,divList){
-    $(divList).each(function(){
-        var div = $(this);
-        var ls = div.find("*[data-field]")
-        $(ls).each(function(){
-           var o = $(this);
-           var k = o.attr("data-field");
-           var v =data[k];
-           if(k == "latestFinanceRound" || k=="runState" || k=="needFinance"){
-               if(!v) o.removeClass(o.attr("class"));
-           }else if(k == "logoSmall"){
-               	if (v && v.indexOf("/")!=-1){
-                    img = v.split("/")[1]
-                    v= '<img src="'+Constants.logoPath+img+'">'
-                }else if (v && v!=""){
-                    img = v
-                    v= '<img src="'+Constants.logoPath+img+'">'
-                }else{
-                    v= '<img src="img/default.gif">'
-                }
-           }else if(k=="projTitle"){
-                if(!v){
-                    v = "名称未知"
-                }
-           }else if(k=="districtSubName"){
-               if(!v){
-                   v="地区未知"
-               }else{
-                   if(v=='国外'){
-                       v='地区未知'
-                   }else{
-                       v = '<span>'+v+'</span>'
-                       if(data.districtGrandsonName){
-                         v +='<span class="dot">·</span><span>'+data.districtGrandsonName+'</span>'
-                       }
-                   }
-               }
-           }else if(k=="industryName"){
-                var str = "";
-                if(v){
-                    str += v;
-                    if(data["industrySubName"]){
-                        str += ">" + data["industrySubName"]
-                    }
-                }else{
-                    str ="行业未知"
-                }
-                v =  str;
-           }else if(k=="labels"){
-                if(v){
-                    var str = "";
-                    $(v.split(",")).each(function(i,e){
-                         str+= "<span class='project_lable'>"+e+"</span>";
-                    })
-                    v = str
-                }
-           } else if(k=="bp"|| k =="hqEmail" || k== "hqTel"){
-                if(!v){
-                    v= "-"
-                }
-           }else if(k=="weibo"){
-                if(v){
-                    v='<a href ="'+v+'" target="_blank"><span class="brain_ico brain_ico_project_1_on"></span></a>'
-                }else{
-                    v='<span class="brain_ico brain_ico_project_1"></span>'
-                }
-           }else if(k=="weixin"){
-               if(v){
-                    o.parent().append('<ul class="weixin_ewm"><li class="wx_img"><img src="https://open.weixin.qq.com/qr/code/?username='+v+'"><span>'+v+'</span>	</li> </ul>')
-                    v='<span class="brain_ico brain_ico_project_2_on"  ></span>'
-               }else{
-                    v='<span class="brain_ico brain_ico_project_2"  title="" ></span>'
-               }
-           }else if(k=="webUrl"){
-                if(v){
-                    v ='<a href ="'+v+'" target="_blank"><span class="brain_ico brain_ico_project_3_on"></span></a>'
-                }else{
-                    v = '<span class="brain_ico brain_ico_project_3"></span>'
-                }
-           }else if(k=="firmDesc"){
-                if(!v){
-                    v ="<span>暂无数据</span>"
-                	 o.css('height','auto')
-                	 o.parent().children('.project_more').hide()
-                }else{
-                	if(v.length <250){
-	            		 o.css('height','auto')
-	                	 o.parent().children('.project_more').hide()
-                	}else {
 
-					}
-                }
-           }
-           o.html(v)
-        })
-    })
-}
 var name;
-var code = getHrefParamter("code");
-sendPostRequest(dataUrl.products+code,callBack.productData);
-sendGetRequest(detail.queryProject+"/"+code,function(data){name =  data.data.projTitle;formatProjectInfo(data.data,$("div[data-query='projectBase']"))})
-sendGetRequest(detail.queryRelativeListByCode+"/"+code,function(data){fillList(data.data,$("div[data-query='list']")) })
+sendPostRequest(dataUrl.products+getHrefParamter("code"),callBack.productData);
+sendGetRequest(detail.queryRelativeListByCode+"/"+getHrefParamter("code"),function(data){fillList(data.data,$("div[data-query='list']")) })
