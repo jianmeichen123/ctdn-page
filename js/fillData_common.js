0d468c772a12d191dd7d4f186d83071e2467447f
wanghaonan <haonanwang@galaxyinternet.com>
2017-07-27 15:20:17 +0800

    修改每个formatter的结果数

--- js/fillData_common.js
+++ js/fillData_common.js
@@ -4,10 +4,50 @@ function fillList(dataList,divList){
         var div = $(this);
         if(dataList){
             var entityList = dataList[div.attr("data-formatter")];
-                    var formatter= div.attr("data-formatter")+"Formatter";
-                    if(formatter && formatter in window){
-                        window[formatter](entityList,div)
-                    }
+            var num = entityList.length;
+            var matter = div.attr("data-formatter")
+            switch(matter){
+                case 'projectMarkInfoList' :
+                //var num = entityList.length;
+                //共有结果个数
+                $(".project_t").find("#mark").text(num)
+                break;
+
+                case 'projectPatentInfoList' :
+               // var num = entityList.length;
+                //共有结果个数
+                $(".project_t").find("#patent").text(num)
+                break;
+
+                case 'projectCopyrightInfoList' :
+                //var num = entityList.length;
+                //共有结果个数
+                $(".project_t").find("#copyright").text(num)
+                break;
+
+                case 'softwareCopyrightInfoList' :
+                //var num = entityList.length;
+                //共有结果个数
+                $(".project_t").find("#software").text(num)
+                break;
+
+                case 'projectWebInfoList' :
+                //var num = entityList.length;
+                //共有结果个数
+                 $(".project_t").find("#web").text(num)
+                 break;
+
+                 case 'certificateInfoList' :
+                 //var num = entityList.length;
+                 //共有结果个数
+                 $(".project_t").find("#cert").text(num)
+                 break;
+            }
+
+            var formatter= div.attr("data-formatter")+"Formatter";
+            if(formatter && formatter in window){
+                window[formatter](entityList,div)
+            }
         }else{
             div.append("暂无数据")
         }
