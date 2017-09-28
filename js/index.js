d8e045c59e30a9f109346874adf3e4f306cc1d12
chunhuanzhang <chunhuanzhang@galaxyinternet.com>
2017-09-28 10:10:48 +0800

    index

--- js/index.js
+++ js/index.js
@@ -85,3 +85,17 @@ function pop_attention(id) {
 		}
     });
 }
+var divList = $(".index_l").find("div[data-block]");
+$(divList).each(function(){
+    var div =$(this);
+    var url = detail[div.attr("data-block")];
+
+    sendGetRequest(url,function(data){
+       $(data.data).each(function(k,v){
+            if(!v)v="-"
+       })
+       var target = $("#"+div.attr("data-block"));
+
+       target.tmpl(data).appendTo(target.parent())
+    })
+})
