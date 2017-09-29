09c6b183deb6846d0b7eeaab6fc3b7ab34510472
chunhuanzhang <chunhuanzhang@galaxyinternet.com>
2017-09-29 18:11:44 +0800

    url

--- js/platformUrl.js
+++ js/platformUrl.js
@@ -1,23 +1,23 @@
 //本地
 
-var Constants = {
-	platformContentURL: "http://127.0.0.1:8081/api/",
-	userContentURL: "http://ctdndev.gi.com/user/",
-	searchContentURL: "http://10.9.130.135:8089/search/",
-	dataContentURL: "http://ctdndev.gi.com/data/",
-	logoPath: "http:///10.10.0.147/"
-}
+//var Constants = {
+//	platformContentURL: "http://127.0.0.1:8081/api/",
+//	userContentURL: "http://ctdndev.gi.com/user/",
+//	searchContentURL: "http://10.9.130.135:8089/search/",
+//	dataContentURL: "http://ctdndev.gi.com/data/",
+//	logoPath: "http:///10.10.0.147/"
+//}
 
 
 
 //线上
-//var Constants = {
-//	platformContentURL: "http://ctdnrc.galaxyinternet.com/api/",
-//	userContentURL: "http://ctdnrc.galaxyinternet.com/user/",
-//	searchContentURL: "http://ctdnrc.galaxyinternet.com/search/",
-//	dataContentURL: "http://ctdnrc.galaxyinternet.com/data/",
-//    logoPath: "http://static.galaxyinternet.com/"
-//}
+var Constants = {
+	platformContentURL: "http://ctdnrc.galaxyinternet.com/api/",
+	userContentURL: "http://ctdnrc.galaxyinternet.com/user/",
+	searchContentURL: "http://ctdnrc.galaxyinternet.com/search/",
+	dataContentURL: "http://ctdnrc.galaxyinternet.com/data/",
+    logoPath: "http://static.galaxyinternet.com/"
+}
 var home = {
 	    index : "http://ctdnrc.galaxyinternet.com"
 	}
