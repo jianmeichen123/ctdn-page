b536af49bbcb7f240e85256d23732e94120593f8
裴 <@10.8.233.228>
2017-11-20 10:37:57 +0800

    banner

--- js/jquery_slide/jquery_slide.js
+++ js/jquery_slide/jquery_slide.js
@@ -1,11 +1,11 @@
 // JavaScript Document
 function i_slide(obj,opt){
   var option={
-		speed:"5000",//间隔运动时间
+		speed:"50000",//间隔运动时间
 		a_speed:"500",//运动时间
 		conuntW:"1520",//整体内容宽度
 		countH:"450",//整体内容高度
-		w1:"840",//大图宽度
+		w1:"800",//大图宽度
 		h1:"450",//大图高度
 		w2:"700",//小图宽度
 		h2:"350"//小图高度  
