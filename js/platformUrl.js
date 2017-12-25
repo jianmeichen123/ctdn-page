0651648398dd6879e76002c5691398ac8489d8ce
chunhuanzhang <chunhuanzhang@galaxyinternet.com>
2017-12-25 20:27:17 +0800

    qa

--- js/platformUrl.js
+++ js/platformUrl.js
@@ -10,12 +10,12 @@
 //}
 
 var Constants = {
-	platformContentURL: "http://ctdndev.gi.com/api/",
-	searchContentURL: "http://ctdndev.gi.com/search/",
-	dataContentURL: "http://ctdndev.gi.com/data/",
-	uploadURL: "http://ctdndev.gi.com/cloudstorage/",
+	platformContentURL: "http://ctdnqa.gi.com/api/",
+	searchContentURL: "http://ctdnqa.gi.com/search/",
+	dataContentURL: "http://ctdnqa.gi.com/data/",
+	uploadURL: "http://ctdnqa.gi.com/cloudstorage/",
 	logoPath: "http://static.galaxyinternet.com/img/",
-	userContentURL: "http://ctdndev.gi.com/user/"
+	userContentURL: "http://ctdnqa.gi.com/user/"
 }
 
 //var Constants = {
