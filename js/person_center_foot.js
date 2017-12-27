962eb99fc4735a31de1115d4cf1ad52d258a6017
chenjianbo <18612802008@163.com>
2017-12-27 17:20:22 +0800

    左侧导航

--- js/person_center_foot.js
+++ js/person_center_foot.js
@@ -1,10 +1,10 @@
-//快速下拉菜单
-var html = '<li><a href="person_center_myproject.html">我的项目</a></li>';
+//左侧菜单
+var html = '<li><span class="first"></span><a href="person_center_myproject.html">我的项目</a></li>';
 if(getCookie("roleCode") && getCookie("roleCode") !="20000"){
-   html+=('<li><a href="concern_industry.html">关注行业</a></li>')
+   html+=('<li><span class="second"></span><a href="concern_industry.html">关注行业</a></li>')
 }
-html+= '<li><a href="person_center_fonder.html">收藏夹</a></li>';
+html+= '<li><span class="third"></span><a href="person_center_fonder.html">收藏夹</a></li>';
 if(getCookie("s_") && getCookie("s_") =="external"){
-	html+= '<li><a href="person_resetpassword.html">修改密码</a></li>';
+	html+= '<li><span class="fourth"></span><a href="person_resetpassword.html">修改密码</a></li>';
 }
 $("#personmenu").html(html)
