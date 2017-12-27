77c0ef52726afbba30532409e20e96ba6c0c4d93
chunhuanzhang <chunhuanzhang@galaxyinternet.com>
2017-12-27 17:55:28 +0800

    菜单

--- js/platformUrl.js
+++ js/platformUrl.js
@@ -10,12 +10,12 @@
 //}
 
 var Constants = {
-	platformContentURL: "http://ctdnqa.gi.com/api/",
-	searchContentURL: "http://ctdnqa.gi.com/search/",
-	dataContentURL: "http://ctdnqa.gi.com/data/",
-	uploadURL: "http://ctdnqa.gi.com/cloudstorage/",
+	platformContentURL: "http://ctdndev.gi.com/api/",
+	searchContentURL: "http://ctdndev.gi.com/search/",
+	dataContentURL: "http://ctdndev.gi.com/data/",
+	uploadURL: "http://ctdndev.gi.com/cloudstorage/",
 	logoPath: "http://static.galaxyinternet.com/img/",
-	userContentURL: "http://ctdnqa.gi.com/user/"
+	userContentURL: "http://ctdndev.gi.com/user/"
 }
 
 //var Constants = {
@@ -35,7 +35,7 @@ var Constants = {
 //    logoPath: "http://static.galaxyinternet.com/img/"
 //}
 var home = {
-	    index : "http://ctdnqa.gi.com"
+	    index : "http://ctdndev.gi.com"
 	}
 var docUrl = {
 
