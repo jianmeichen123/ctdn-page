8866229b99cfc969f6b13a62237d9206b9401430
zsl <rezhangsl@163.com>
2018-01-03 21:12:50 +0800

    update bug

--- js/person_particulars.js
+++ js/person_particulars.js
@@ -23,28 +23,35 @@
     function loadMore(more,obj){
         var url = obj.attr("data-url");
         var dataId = obj.attr("data-id");
-        var type = obj.attr("type");
+        //是否分页 投资人分页 创业者不分页
+        var type=obj.attr("personType");
         url = getUrl(dataId,url);
 		var json = getJson(obj);
     	var pageNo = obj.find("input[name='pageNo']").val();
     	var pageSize = obj.find("input[name='pageSize']").val();
-
     	var html="";
     	sendPostRequestByJsonObj(url,json,function(data){
-    		var records = data.page.records;
 
-    		if(records.length>0 ){
+    		if(data.page&&data.page.records.length>0 ){
     		    obj.show();
+                                obj.addClass('storey_list');
+                                var scroll_on = obj.attr('scroll_on');
+                                $('.project_nav [scroll_on="'+scroll_on+'"]').addClass('scroll_on');
+                                $('.project_nav [scroll_on="'+scroll_on+'"]').show();
+                                $('.project_nav').show();
     		    var target = $("#"+dataId)
-    		    target.tmpl(records).appendTo(target.parent())
+    		    target.tmpl(data.page).appendTo(target.parent())
 				if(pageNo && pageSize){
-				    if(data.page.total<=(pageNo*1)*pageSize){
+				    if(data.page.total<=(pageNo+1)*pageSize){
                         more.hide();
                         return;
                     }
+
                     pageNo = pageNo*1+1
                     obj.find("input[name='pageNo']").val(pageNo)
 				}
+    		}else{
+    		     obj.hide();
     		}
     	})
    }
@@ -52,6 +59,7 @@
    function loadNoPage(obj){
         var url = obj.attr("data-url");
         var dataId = obj.attr("data-id")
+        var type=obj.attr("personType");
         //data-id是渲染数据的模板id,如果没有该属性,说明该模块不需要渲染数据
         if(!dataId){
             return;
@@ -59,8 +67,10 @@
         url = getUrl(dataId,url);
         var json = getJson(obj);
         sendPostRequestByJsonObj(url,json,function(data){
+
             if(data.data !='' && data.data!=null){
                 var target = $("#"+dataId)
+
                if(data.data instanceof Array){
                     target.tmpl(data).appendTo(target.parent())
                 }else{
