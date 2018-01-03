a77b8b6a58962f7a5aafa7ca6a307cdb671d0c0d
chunhuanzhang <chunhuanzhang@galaxyinternet.com>
2018-01-03 19:31:03 +0800

    pageSize 8

--- js/collection.js
+++ js/collection.js
@@ -32,8 +32,7 @@ function loadTable(tab){
         tableDataTotalName:'total',
         sidePagination: "server",           //分页方式：client客户端分页，server服务端分页（*）
         pageNumber: 1,                       //初始化加载第一页，默认第一页
-        pageSize: 10,                       //每页的记录行数（*）
-        pageList: [15,20,30],        //可供选择的每页的行数（*）
+        pageSize: 8,                       //每页的记录行数（*）
         formatLoadingMessage: function () {
             return "请稍等，正在加载中...";
         },
@@ -110,7 +109,7 @@ function initTable() {
   	//初始化Table
   	oTableInit.Init = function () {
   	        $('table[data-type]').bootstrapTable({
-               		pageSize:15,
+               		pageSize:8,
                		showRefresh : false ,
                		sidePagination: 'server',
                		method : 'post',
